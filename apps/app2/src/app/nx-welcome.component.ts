a12c9c78f2b6a78564dadbdc0a45c58c021210c5
Bob Tracy <retracy@live.com>
2022-12-15 17:56:35 -0800

    Created two applications

---
+++ apps/app2/src/app/nx-welcome.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'nxwork-nx-welcome',
+  template: `
+    <!--
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     This is a starter component and can be deleted.
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     Delete this file and get started with your project!
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     -->
+    <div class="wrapper">
+      <div class="container">
+        <!--  WELCOME  -->
+        <div id="welcome">
+          <h1>
+            <span> Hello there, </span>
+            Welcome app2 👋
+          </h1>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent {}
